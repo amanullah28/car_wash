9cd5dd12df492159b3d239e71075c15792776968
Md Amanullah Sabir <md.amanullah@neosoftmail.com>
2021-06-27 13:27:13 +0530

    user registration

---
+++ helper/userType.js
@@ -0,0 +1,5 @@
+module.exports =  {
+    ADMIN: 1,
+    CAR_WASHER: 2, 
+    CUSTOMER: 3
+}
--- services/add-washer.js
+++ services/add-washer.js
@@ -1,21 +1,55 @@
-const util = require('../util');
+const util = require('../helper/util');
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
 const DocClient = new AWS.DynamoDB.DocumentClient();
-const tableName = process.env.CAR_WASHER_TABLE;
+const table = process.env.CAR_WASHER_TABLE;
 const { v4: uuidv4 } = require('uuid');
+const uType = require('../helper/userType');
+const bcrypt = require('bcrypt');
+
 
 exports.handler = async (event, ctxt)=>{
+    
     try {
-        let item = JSON.parse(event.body);
+        let input = JSON.parse(event.body);
+        let hashPassword = await bcrypt.hash(input.password, 10);
+        let readParams = {
+            TableName: table,
+            FilterExpression : "email = :e",
+            ExpressionAttributeValues : {
+                ":e": input.email
+            }
+        }
+
+        let result = await DocClient.scan(readParams).promise();
+
+        if(result.Items.length>0) {
+            throw new Error("User Already Exist with the given email!!")
+        }
+
+        let params = {
+            TableName: table,
+            Item: {
+                washerId: uuidv4(),
+                email: input.email,
+                password: hashPassword,
+                name: input.name,
+                userType: uType.CAR_WASHER,
+                isActive: true,
+                address: input.address,
+                createdAt: Date.now()
+            }
+        };
+
+        await DocClient.put(params).promise();
 
         return {
             statusCode: 200,
             headers: util.getResponseHeader(),
             body: JSON.stringify({
-                name: item.name,
-                email: item.email,
-                data: item
+                message: "User Created Successfully!!",
+                tableName: table,
+                data: input
             })
         }
     } catch(err) {
@@ -25,7 +59,8 @@ exports.handler = async (event, ctxt)=>{
             headers: util.getResponseHeader(),
             body: JSON.stringify({
                 error: err.name ? err.name : "Exception",
-                message: err.message ? err.message : "Unknown Error"
+                message: err.message ? err.message : "Unknown Error",
+                errorBody: err.stack
             })
         }
     }
